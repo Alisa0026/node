bba46c3d08540c9e3b6c8215cd6951d87918fa97
alisa0026 <943578013@qq.com>
2016-11-26 18:16:27 +0800

    修改events

--- 20161126_node/4_events/1_events.js
+++ 20161126_node/4_events/1_events.js
@@ -18,20 +18,33 @@ Man.prototype.on = function(eventName,callback){
 };
 //触发
 Man.prototype.emit = function(eventName){
-    this.events[eventName].forEach(function(item){
-        item();
-    })
+    //截取除了事件名第一项其他的组成一个数组，传递给item执行
+    //var args = [].slice.call(arguments,1);
+    var args = Array.from(arguments).slice(1);
+
+    if(this.events[eventName]){
+        //箭头函数，它里面没有this指向，指向的就是上一级的this
+        this.events[eventName].forEach((item) => {
+            //console.log(this);
+            item.apply(null,args);
+        })
+    }
 };
 
-function buyPack(){
-    console.log("买包");
+function buyPack(who){
+    console.log("买包给" + who);
 }
-function buyCar(){
-    console.log("买车");
+function buyCar(who){
+    console.log("买车给" + who);
 }
 
 var man = new Man();
+/*
 man.on('有钱了',buyPack);
 man.on('有钱了',buyCar);
 man.emit('有钱了');
-console.log(man.events)
+*/
+//作业
+man.on('有钱了',buyPack);
+man.on('有钱了',buyCar);
+man.emit('有钱了','妹子','朋友'); //参数不确定
